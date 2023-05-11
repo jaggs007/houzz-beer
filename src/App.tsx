a5708b5f3ba8bbd5f03c5e8d507ae7659538832b
Jagaran Shrestha <jagaran.shrestha9@gmai.com>
2023-05-11 17:13:16 +0545

    Refactor routes for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
 import { Container } from "react-bootstrap";
-import Beers from "./components/Beers";
-import CustomBeers from "./components/CustomBeers";
+import router from "./routes";
 
 function App() {
   return (
     <div className="App">
       <Container>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route>
-              <Route path="/" element={<Beers />} />
-              <Route path="all-beers" element={<Beers />} />
-              <Route path="custom-beers" element={<CustomBeers />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </Container>
     </div>
   );
---
+++ src/routes.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import Beers from "./components/Beers";
+import CustomBeers from "./components/CustomBeers";
+import Header from "./components/Header";
+
+const router = createBrowserRouter([
+  {
+    element: <Header />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/all-beers" replace />,
+      },
+      {
+        path: "/all-beers",
+        element: <Beers />,
+      },
+      {
+        path: "/custom-beers",
+        element: <CustomBeers />,
+      },
+    ],
+  },
+]);
+
+export default router;
