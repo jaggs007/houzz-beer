cc381ba055821257fee5f13be90b80cbf00cd37c
Jagaran Shrestha <jagaran.shrestha9@gmai.com>
2023-05-11 17:08:15 +0545

    Add beer slice for store

---
+++ src/store/beersSlice.ts
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { BeerResponseT } from "../types";
+import { BEERS_API } from "../constants";
+
+export interface BeerState {
+  data: BeerResponseT[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: BeerState = {
+  data: [],
+  loading: false,
+  error: null,
+};
+
+export const fetchBeers = createAsyncThunk(
+  "beers/fetchBeers",
+  async (queryParams: { page: number; itemsPerPage: number }) => {
+    const { page, itemsPerPage } = queryParams;
+    const response = await fetch(
+      `${BEERS_API}?page=${page}&per_page=${itemsPerPage}`
+    );
+    const data = await response.json();
+    return data as BeerResponseT[];
+  }
+);
+
+export const beerSlice = createSlice({
+  name: "beers",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBeers.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchBeers.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = [...state.data, ...action.payload];
+      })
+      .addCase(fetchBeers.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message ?? "Something went wrong";
+      });
+  },
+});
+
+export default beerSlice.reducer;
