88cb303e5e88d52f19d73b8950d0611c384f652d
Jagaran Shrestha <jagaran.shrestha9@gmai.com>
2023-05-11 14:05:15 +0545

    Add component to display beers created manually

---
+++ src/components/CustomBeers/index.tsx
@@ -0,0 +1,58 @@
+import { Col, Container, Row } from "react-bootstrap";
+import CustomBeerModal from "../AddBeerModal";
+import { useModal } from "../../hooks";
+import { CustomBeerItemCard } from "./CustomBeerItem";
+import { useSelector } from "react-redux";
+import { CustomBeerT } from "../../types";
+
+const CustomBeers = () => {
+  const { isOpen, onOpenModal, onCloseModal } = useModal();
+
+  const customBeers = useSelector((state: any) => {
+    return state.customBeers.customBeers;
+  });
+
+  return (
+    <Container className="d-flex flex-column">
+      <CustomBeerModal isOpen={isOpen} onCloseModal={onCloseModal} />
+      {!customBeers || customBeers.length === 0 ? (
+        <EmptyContainer onOpenModal={onOpenModal} />
+      ) : (
+        <Container>
+          {customBeers.map((beer: CustomBeerT) => (
+            <CustomBeerItemCard beer={beer} key={beer.name} />
+          ))}
+        </Container>
+      )}
+    </Container>
+  );
+};
+
+export default CustomBeers;
+
+const EmptyContainer = ({ onOpenModal }: any) => {
+  return (
+    <Container>
+      <Row>
+        <Col xs={12}>Nothing to see yet</Col>
+        <Col xs={12}>
+          <Row className="justify-content-center">
+            <Col>
+              <p className="text-center">
+                <a
+                  className="link-primary"
+                  onClick={onOpenModal}
+                  href="#"
+                  role="button"
+                >
+                  Click here
+                </a>{" "}
+                to add your first beer
+              </p>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
